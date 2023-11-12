385d13e12211761b361aaec86da13a25cc88c2c8
Gurram Nikhil <gurram.nikhil@outlook.com>
2023-11-12 19:00:42 +0530

    Ex 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,27 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
 
     <div>
       <Header name={course} />
-      <Content parts={[part1.name,part2.name,part3.name]} exercises={[part1.exercises,part2.exercises,part3.exercises]} />
+      <Content parts={[parts[0].name,parts[1].name,parts[2].name]} exercises={[parts[0].exercises,parts[1].exercises,parts[2].exercises]} />
       
-      <Total number={[part1.exercises,part2.exercises,part3.exercises]} />
+      <Total number={[parts[0].exercises,parts[1].exercises,parts[2].exercises]} />
     </div>
   )
 }
