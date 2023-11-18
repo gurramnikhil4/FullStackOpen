ecf9073dce153cf8738f00d5a22b6c4ca148c47c
Gurram Nikhil <gurram.nikhil@outlook.com>
2023-11-18 15:05:13 +0530

    Ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,34 +6,36 @@ const Button = (props) =>{
   )
 }
 
+const Statistics = ({ratings,isGiven}) =>{
+  const all=ratings[0]+ratings[1]+ratings[2];
+  return(
+    <>
+      <h1>Statistics</h1>
+      <p>good {ratings[0]}</p>
+      <p>neutral {ratings[1]}</p>
+      <p>bad {ratings[2]}</p>
+      <p>all {all}</p>
+      <p>average {all?((ratings[0]-ratings[2])/all):0}</p>
+      <p>positve {all?((ratings[0]*100)/all):0}</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll]=useState(0)
-
-  const onSmash = (setRating) =>{
-    setAll(n=>n+1);
-    setRating(n=>n+1);
-  }
-
-const average = all?((good-bad)/all):0;
+  const [isGiven, setGiven] = useState(0)
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={()=>onSmash(setGood)}text="good"/>
-      <Button handleClick={()=>onSmash(setNeutral)}text="neutral"/>
-      <Button handleClick={()=>onSmash(setBad)}text="bad"/>
+      <Button handleClick={()=>setGood(n=>n+1)}text="good"/>
+      <Button handleClick={()=>setNeutral(n=>n+1)}text="neutral"/>
+      <Button handleClick={()=>setBad(n=>n+1)}text="bad"/>
 
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-
+     <Statistics ratings={[good,neutral,bad]}/>
 
     </div>
   )
