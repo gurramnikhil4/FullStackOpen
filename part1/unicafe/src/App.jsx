4986b8d74b19dae6c5c5d7f57bb4fa42f04b5bc1
Gurram Nikhil <gurram.nikhil@outlook.com>
2023-11-18 13:45:30 +0530

    Ex 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,18 +11,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll]=useState(0)
+
+  const onSmash = (setRating) =>{
+    setAll(n=>n+1);
+    setRating(n=>n+1);
+  }
+
+const average = all?((good-bad)/all):0;
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={()=>setGood(n=>n+1)}text="good"/>
-      <Button handleClick={()=>setNeutral(n=>n+1)}text="neutral"/>
-      <Button handleClick={()=>setBad(n=>n+1)}text="bad"/>
+      <Button handleClick={()=>onSmash(setGood)}text="good"/>
+      <Button handleClick={()=>onSmash(setNeutral)}text="neutral"/>
+      <Button handleClick={()=>onSmash(setBad)}text="bad"/>
 
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+
 
     </div>
   )
