37e03f977ff8ec12ab5ed28b9bc3959c71d6a864
Gurram Nikhil <gurram.nikhil@outlook.com>
2023-11-22 23:48:23 +0530

    Ex 2.1 Define course component

---
+++ part2/src/App.jsx
@@ -0,0 +1,60 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course=({course})=>{
+  return(
+    <>
+    <Header name={course.name}></Header>
+    <Content parts={course.parts}></Content>
+    </>
+  )
+
+}
+
+const Header=({name})=>{
+  return(
+    <h1>{name}</h1>
+  )
+  }
+  
+  const Content=({parts})=>{
+    return(
+      <>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
+      </>
+    )
+  }
+  
+  
+  const Part=({part})=>{
+    return(
+      <p>{part.name} {part.exercises}</p>
+    )
+    }
+
+export default App
