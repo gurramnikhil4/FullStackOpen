e8c4aed1cfa99556d6d13cda5b2ba4dec55ba84a
Gurram Nikhil <gurram.nikhil@outlook.com>
2024-02-05 23:45:08 +0530

    Ex 4.5 Add Favourite Blog helper and tester

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,7 +8,25 @@ const totalLikes =(blogs)=>{
 		return sum+curr.likes
 	},0)
 }
+
+const favoriteBlog =(blogs)=>{
+
+if(blogs.length==0)return {}
+
+ let fBlog={
+	'likes':0
+ }
+
+ for(let i=0;i<blogs.length;i++){
+	if(blogs[i].likes>=fBlog.likes){
+		fBlog.title=blogs[i].title
+		fBlog.author=blogs[i].author
+		fBlog.likes=blogs[i].likes
+	}
+ }
+ return fBlog
+}
   
   module.exports = {
-	dummy, totalLikes
+	dummy, totalLikes, favoriteBlog
   }
