d4f54b09982f661b5d76a42a2ae2854323414536
Gurram Nikhil <gurram.nikhil@outlook.com>
2024-02-05 23:17:58 +0530

    Ex 4.4 Add Total like tester

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
  	return 1  
 }
+
+const totalLikes =(blogs)=>{
+
+	return blogs.reduce((sum,curr)=>{
+		return sum+curr.likes
+	},0)
+}
   
   module.exports = {
-	dummy
+	dummy, totalLikes
   }
