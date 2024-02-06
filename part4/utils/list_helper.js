1a391e95faeb7f3604bef458cd8ae97e7b46203f
Gurram Nikhil <gurram.nikhil@outlook.com>
2024-02-06 22:08:50 +0530

    Ex 4.7 Implement mostLiked

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,6 @@
+const blog = require("../models/blog")
+const _ = require('lodash')
+
 const dummy = (blogs) => {
  	return 1  
 }
@@ -10,23 +13,54 @@ const totalLikes =(blogs)=>{
 }
 
 const favoriteBlog =(blogs)=>{
+	if(blogs.length==0)return {}
 
-if(blogs.length==0)return {}
-
- let fBlog={
-	'likes':0
- }
-
- for(let i=0;i<blogs.length;i++){
-	if(blogs[i].likes>=fBlog.likes){
-		fBlog.title=blogs[i].title
-		fBlog.author=blogs[i].author
-		fBlog.likes=blogs[i].likes
+	let fBlog={
+		'likes':0
 	}
- }
- return fBlog
+
+	for(let i=0;i<blogs.length;i++){
+		if(blogs[i].likes>=fBlog.likes){
+			fBlog.title=blogs[i].title
+			fBlog.author=blogs[i].author
+			fBlog.likes=blogs[i].likes
+		}
+	}
+ 	return fBlog
 }
   
+
+const mostLikes =(blogs)=>{
+	let map={}
+	for(entry in blogs){
+
+		if(map[blogs[entry].author])
+		map[blogs[entry].author]+=blogs[entry].likes
+		
+		else
+		map[blogs[entry].author]=blogs[entry].likes
+
+	}
+	console.log(map)
+
+	let mostLiked={ 
+		"author": null,
+		"likes":0
+	}
+	
+	_.forEach(map, (value,key)=>{
+		if(mostLiked.likes<value){
+			mostLiked.author=key
+			mostLiked.likes=value
+		}
+	})
+
+	return mostLiked
+}
+
   module.exports = {
-	dummy, totalLikes, favoriteBlog
+	dummy, 
+	totalLikes,
+	favoriteBlog,
+	mostLikes
   }
